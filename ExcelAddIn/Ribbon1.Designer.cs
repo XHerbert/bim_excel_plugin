a7f981509c970158e4ca161742395ff154a972e6
徐洪波 <xuhb-c@glodon.com>
2019-09-08 21:12:03 +0800

    Initial commit

---
+++ ExcelAddIn/Ribbon1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ExcelAddIn
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Office.Tools.Ribbon.RibbonDialogLauncher ribbonDialogLauncherImpl1 = this.Factory.CreateRibbonDialogLauncher();
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group = this.Factory.CreateRibbonGroup();
+            this.systemGroup = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.Groups.Add(this.group);
+            this.tab1.Label = "BIM应用";
+            this.tab1.Name = "tab1";
+            this.tab1.Position = this.Factory.RibbonPosition.BeforeOfficeId("TabInsert");
+            // 
+            // group
+            // 
+            this.group.DialogLauncher = ribbonDialogLauncherImpl1;
+            this.group.Items.Add(this.systemGroup);
+            this.group.Label = "数据库";
+            this.group.Name = "group";
+            this.group.DialogLauncherClick += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.group1_DialogLauncherClick);
+            // 
+            // systemGroup
+            // 
+            this.systemGroup.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.systemGroup.Label = "系统分组";
+            this.systemGroup.Name = "systemGroup";
+            this.systemGroup.OfficeImageId = "Call";
+            this.systemGroup.ShowImage = true;
+            this.systemGroup.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.system_group_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group.ResumeLayout(false);
+            this.group.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton systemGroup;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ ExcelAddIn/Ribbon1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Excel = Microsoft.Office.Interop.Excel;//using Excel
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace ExcelAddIn
+{
+    public partial class Ribbon1
+    {
+
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            //TODO:加载初始化数据，如数据库连接
+            string appsetting = ConfigurationSettings.AppSettings["excel"].ToString();
+            
+        }
+
+        private void system_group_Click(object sender, RibbonControlEventArgs e)
+        {
+            //声明一个Excel的单元格区域变量。
+            Excel.Range rang;
+
+            //获得Excel所选区域
+            rang = Globals.ThisAddIn.Application.Selection;
+
+            //所选区域底纹颜色设置为黄色。
+            //rang.Interior.Color = System.Drawing.Color.Yellow;            
+            int columns = rang.Columns.Count+1;
+            int rows = rang.Rows.Count+1;
+            
+            // 遍历时先列后行，否则会出现矩阵反转
+            for (int i = 1; i < columns; i++)
+            {
+                for (int k = 1; k < rows; k++)
+                {
+                    Excel.Range cell  = rang.Item[i][k];
+                    if(cell.Value2 == null)
+                    {
+                        continue;
+                    }
+                    string cellValue = ((string)cell.Value2).ToString();
+                    if (!cellValue.Contains(','))
+                    {
+                        cell.Value2 = "'" + cellValue.Trim() + "'";
+                    }else
+                    {
+                        cellValue = cellValue.Replace(",", "','");
+                        cell.Value2 = "'" + cellValue.Trim() + "'";
+                    }
+                    
+                }
+            }
+            
+        }
+
+        private void group1_DialogLauncherClick(object sender, RibbonControlEventArgs e)
+        {
+            Form1 fm1 = new Form1();//创建新实例。
+            fm1.ShowDialog();//以对话框的形式，在Excel中显示Form1。
+        }
+    }
+}
